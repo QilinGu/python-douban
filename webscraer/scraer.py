da1cc5dd94de1eb546cee74c93850edf1a6ef53d
tao <tao@localhost.localdomain>
2015-01-05 22:58:06 +0800

    add score

--- webscraer/scraer.py
+++ webscraer/scraer.py
@@ -1,7 +1,7 @@
-# -*- coding: gbk -*-
+# -*- coding: utf-8 -*-
 import sys
 reload(sys)
-sys.setdefaultencoding('gbk')
+sys.setdefaultencoding('utf8')
 import urllib2
 from bs4 import BeautifulSoup
 import re
@@ -13,6 +13,11 @@ if __name__ == '__main__':
 	list1 = []
 	for tag in list0:
 		str0 = tag.find('h3').find('a').text
-		list1.append([str0])
+		str1 = tag.find('div', {'class': 'star clearfix'}).text
+		list1.append([str0, str1])
 	for d in list1:
-		print d[0]
+		# 如果歌曲评分超过8.5，推荐给我
+		if float(d[1]) >= 8.5:
+			# 以发送邮件形式通知我
+			# TODO
+			print(d[0])
