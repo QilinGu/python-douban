81fbd844f1f27a17faf1b9c77e4b5914ea80f428
cls1991 <tzk19910406@hotmail.com>
2015-01-05 18:44:13 +0800

    modify charset

--- webscraer/scraer.py
+++ webscraer/scraer.py
@@ -9,11 +9,10 @@ import re
 if __name__ == '__main__':
 	content0 = urllib2.urlopen(r'http://music.douban.com/').read()
 	soup = BeautifulSoup(content0)
-	list0 = soup.find('ul', {'id': 'newcontent0'})
-	list2 = list0.findAll('li', {'class': 'clearfix'})
-	list3 = list()
-	for tag in list2:
-		str0 = tag.find('h3').find('a').text.encode('gbk', 'ignore')
-		print str0
-		list3.append([str0])
-	print(list3)
+	list0 = soup.find('ul', {'id': 'newcontent0'}).findAll('li', {'class': 'clearfix'})
+	list1 = []
+	for tag in list0:
+		str0 = tag.find('h3').find('a').text
+		list1.append([str0])
+	for d in list1:
+		print d[0]
